142c1a4bdc9c20eca79f6f40389ef18a24c6b644
paulkim172 <55893592+paulkim172@users.noreply.github.com>
2020-09-04 10:58:00 -0400

    created display and button components

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Display from './basicComponents/display';
+import Buttons from './basicComponents/buttons';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Display/>
+      <Buttons/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/basicComponents/buttons.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Buttons extends Component {
+    render() {
+        return (
+            <div>buttons here</div>
+        )
+    }
+}
+
+export default Buttons;
